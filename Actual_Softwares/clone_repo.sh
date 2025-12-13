d80006448c41ca744e2e2b2f9960056a9bd0a9e1
markcyril28 <mercadomarkcyril28@gmail.com>
2025-12-13 12:56:55 +0800

    Added the cloning scripts for the Actual_Softwares folder.

---
+++ Actual_Softwares/clone_repo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Clone alignment and quantification tools
+
+git clone https://github.com/BenLangmead/bowtie2.git
+git clone https://github.com/DaehwanKimLab/hisat2.git
+git clone https://github.com/COMBINE-lab/salmon.git
